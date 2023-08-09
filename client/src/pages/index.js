70df3ec53dd6ab9bb22d5876950145e6396ca802
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2023-08-09 11:38:20 +0700

    update

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,30 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function MyComponent() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     // Trong useEffect, bạn có thể gọi API sử dụng Axios hoặc các phương thức khác trả về Promise
-    axios
-      .get("http://localhost:5000/generator")
-      .then((response) => {
-        setData(response.data); // Lưu kết quả vào state data
-        setLoading(false); // Đã hoàn thành Promise, không cần hiển thị thông báo tải nữa
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false); // Có lỗi, không cần hiển thị thông báo tải nữa
-      });
   }, []);
-  console.log(loading);
-
-  if (loading) {
-    return <div>Loading...</div>; // Hiển thị thông báo đang tải trong khi Promise đang xử lý
-  }
+  console.log();
 
   // Khi Promise hoàn thành, hiển thị dữ liệu đã nhận được từ API
-  return <div>id:{data[0]._id}</div>;
+  return <div>id AA</div>;
 }
 
 export default MyComponent;
--- server/src/v1/router.js
+++ server/src/v1/router.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("./controllers/adminController");
-const generator = require("./controllers/generatorController");
-const motor = require("./controllers/motorController");
-const waterpump = require("./controllers/waterpumpController");
 
 let initWebRoutes = (app) => {
   //
   router.post("/login", adminController.login);
   router.post("/update", adminController.update);
   router.post("/forget", adminController.forget);
-  //
-  router.get("/generator", generator.findall);
-  router.post("/generator/add", generator.add);
-  router.post("/generator/update", generator.update);
-  //
-  router.get("/motor", motor.findall);
-  router.post("/motor/add", motor.add);
-  router.post("/motor/update", motor.update);
-  //
-  router.get("/waterpump", waterpump.findall);
-  router.post("/waterpump/add", waterpump.add);
-  router.post("/waterpump/update", waterpump.update);
 
   return app.use("/", router);
 };
