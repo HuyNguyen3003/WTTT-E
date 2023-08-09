5b532f0dc090459c409054392a99edf3c87b732d
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2023-08-09 19:02:50 +0700

    updateFE

---
+++ client/src/pages/lienhe/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+export default function LienHe() {
+  return (
+    <>
+      <Head>
+        <title>LIÊN HỆ-TÂN THÀNH TÀI</title>
+        <meta
+          name="description"
+          content="Giới thiệu chi tiết về công ty Tân Thành Tài"
+          key="desc"
+        />
+      </Head>
+      <div>LienHe</div>
+    </>
+  );
+}
---
+++ client/src/pages/tintuc/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+export default function TinTuc() {
+  return (
+    <>
+      <Head>
+        <title>TIN TỨC-TÂN THÀNH TÀI</title>
+        <meta
+          name="description"
+          content="Giới thiệu chi tiết về công ty Tân Thành Tài"
+          key="desc"
+        />
+      </Head>
+      <div>TinTuc</div>
+    </>
+  );
+}
